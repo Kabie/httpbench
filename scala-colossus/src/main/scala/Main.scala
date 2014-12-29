2a9b2975e9dfa81d2da359583a21764de57c230b
Kabie <kabie3000@gmail.com>
2014-12-29 11:22:51 +0800

    return json

--- scala-colossus/src/main/scala/Main.scala
+++ scala-colossus/src/main/scala/Main.scala
@@ -3,12 +3,16 @@ import service._
 import protocols.http._
 import UrlParsing._
 import HttpMethod._
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
 
 object Main extends App {
 	implicit val ioSystem = IOSystem()
 
 	Service.become[Http]("http-echo", 9000) {
-		case request @ Get on Root => request.ok("Hello world!")
-		case request @ Get on Root / str => request.ok(str)
+		case request @ Get on Root =>
+			request.ok("""{"Hello":"world!"}""")
+		case request @ Get on Root / str =>
+			request.ok(compact(render(("Hello" -> str))))
 	}
 }
